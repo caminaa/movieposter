61b7bac22fd45ff852d659ab626f1c4f2db9afd2
adrien.caminade <adrien.caminade@y-nov.com>
2015-03-13 12:00:55 +0100

    secondActivity

--- MoviePoster/app/src/main/java/dev/movieposter_team/local/movieposter/SingleMovieActivity.java
+++ MoviePoster/app/src/main/java/dev/movieposter_team/local/movieposter/SingleMovieActivity.java
@@ -1,7 +1,9 @@
 package dev.movieposter_team.local.movieposter;
 
+import android.app.Activity;
+
 /**
  * Created by Adrien on 13/03/2015.
  */
-public class SingleMovieActivity {
+public class SingleMovieActivity extends Activity{
 }
